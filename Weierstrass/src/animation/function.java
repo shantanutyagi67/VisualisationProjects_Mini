a0fa7c559d074c714507686f4b3fce9b093b79fc
Shantanu Tyagi <shantanutyagi67@gmail.com>
2021-06-22 17:34:46 +0530

    wierestrass update

--- Weierstrass/src/animation/function.java
+++ Weierstrass/src/animation/function.java
@@ -17,15 +17,15 @@ public class function extends JComponent implements Runnable{
 	static Vector<Double> Y = new Vector<Double>();
 	double X =0;
 	static double a =0.5;
-	static double b = 0.1;
-	static int range = 701;
-	static double sample = 0.01;
+	static double b = 0.7;
+	static int range = 5001;
+	static double scale = 2;
 	public static void main(String args[]) {
 		for(int i=0;i<range;i++) {
 			Y.add((double) 0);
 		}
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(0,0,800,800);
+		frame.setBounds(0,0,1600,800);
 		frame.getContentPane().add(new function());
 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.setVisible(true);
@@ -34,24 +34,25 @@ public class function extends JComponent implements Runnable{
 	public void paint(Graphics g)
 	{
 		Graphics2D g2D = (Graphics2D) g;
-		g2D.translate(frame.getWidth()/2, frame.getHeight()/2);		
+		g2D.translate(frame.getWidth()/2, 200);		
 		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
     	rh.put(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
     	g2D.setRenderingHints(rh);
 		g2D.setColor(Color.WHITE);
 		for(int i=0;i<range;i++) {	
 			Y.set(i, calculate(i));
-			g2D.fill(new Ellipse2D.Double(X-0.5,Y.elementAt(i)-0.5, 1, 1));
+			g2D.fill(new Ellipse2D.Double((i - (range-1)/2)/4,scale*Y.elementAt(i)-1, 2, 2));
 		}
-		b+=0.001;
+		b+=0.0025;
+		//scale-=0.01;
 		run();
 		//repaint();
 	}
 	
 	private double calculate(double i) {
-		X = i - range/2 + i*sample;
+		X = (i - (range-1)/2)/(range-1)/2;
 		double sum=0;
-		for(int j=0;j<100;j++) {
+		for(int j=0;j<5;j++) {
 			sum+= Math.pow(a, j)*Math.cos(Math.PI*X*Math.pow(b, j));
 		}
 		return 100*sum;
@@ -60,7 +61,7 @@ public class function extends JComponent implements Runnable{
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(10);
+			Thread.sleep(1);
 			repaint();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
